27431366a1a9e1e9304b17a9ab8211530380abcb
Yana Naydenova <yana.naydenova00@gmail.com>
2024-10-18 15:54:07 +0300

    Create activity_sign_in.xml

--- app/src/main/java/com/example/mixit/MainActivity.java
+++ app/src/main/java/com/example/mixit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mixit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    }
+
+        Button button_Start = (Button) findViewById(R.id.StartButton);
+
+        button_Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        SignIn.class);
+                startActivity(myIntent);
+            }
+        });
+
+    };
+
 }
