e6c17d7b359c8d48e65716a5d8df034af618ae32
Yana Naydenova <yana.naydenova00@gmail.com>
2024-10-20 22:18:49 +0300

    Create edit_text_background.xml Create line under text "log in with" and button "forgot your password"

--- app/src/main/java/com/example/mixit/SignIn.java
+++ app/src/main/java/com/example/mixit/SignIn.java
@@ -1,6 +1,14 @@
 package com.example.mixit;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.UnderlineSpan;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +22,23 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button buttonForgotYourPassword =(Button) findViewById(R.id.buttonForgotPassword);
+        TextView textLogInWith=(TextView)findViewById(R.id.textLogInWith);
+
+        // Line under textView "textLogInWith"
+        String textLogwithLine = (String) textLogInWith.getText();
+        SpannableString LineForTextLogWith =new SpannableString(textLogwithLine);
+        LineForTextLogWith.setSpan(new UnderlineSpan(), 0,textLogwithLine.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textLogInWith.setText(LineForTextLogWith);
+
+        // Line under text in the button "buttonForgotPassword"
+        String textForgotYourPassword = (String) buttonForgotYourPassword.getText();
+        SpannableString LineForForgotButton =new SpannableString(textForgotYourPassword);
+        LineForForgotButton.setSpan(new UnderlineSpan(), 0,textForgotYourPassword.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        buttonForgotYourPassword.setText(LineForForgotButton);
+
+
 
     }
 }
